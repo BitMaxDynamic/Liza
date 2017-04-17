d4fe78abfebffee8429eace984af7909430f9f4e
Ilya Shusterman <ilyashusterman@outlook.com>
2017-04-18 02:20:21 +0300

    minor code fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { MaterialModule } from '@angular/material';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot(),
+    MaterialModule,
     AppRoutingModule,
     ReactiveFormsModule
   ],
